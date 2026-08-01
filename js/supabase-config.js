2b3ddacfcb43eee2a3237949c1d6e0e0ea957568
pranavgovindans <pranavgovindans@gmail.com>
2026-08-01 11:31:51 +0530

    Add VitaWell wellbeing features

---
+++ js/supabase-config.js
@@ -0,0 +1,5 @@
+// Public client configuration for VitaWell. This key is safe for browser use.
+window.VITAWELL_SUPABASE = {
+  url: 'https://ocbyojhheqwtqwawuuhh.supabase.co',
+  publishableKey: 'sb_publishable_mzlxH_1409IShaEVXLvrvQ_LUZlR01R'
+};
